1b710ad2afb81c82d3ce953451ec2e4634b809ad
Johan Segerfeldt <johan.segerfeldt@me.com>
2018-08-14 21:52:15 +0200

    Reorder the logic
    … and use rawValue to find the Verb case

--- Sources/HTTP/RequestParser.swift
+++ Sources/HTTP/RequestParser.swift
@@ -4,14 +4,9 @@ public class RequestParser {
 	public init() {}
 
 	public func request(parsing input: String) throws -> Request {
-		if input.hasPrefix(Verb.get.rawValue) {
-			return Request(verb: .get)
-		}
-		if input.hasPrefix(Verb.post.rawValue) {
-			return Request(verb: .post)
-		}
-
-		throw RequestParserError.some
+		let verbString = String(input.prefix { $0 != " " })
+		guard let verb = Verb(rawValue: verbString) else { throw RequestParserError.some }
+		return Request(verb: verb)
 	}
 }
 
