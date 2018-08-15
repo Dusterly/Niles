2ab886ff9744f14b7186e96585755d0667486992
Johan Segerfeldt <johan.segerfeldt@me.com>
2018-08-15 22:32:17 +0200

    Add list of standard verbs

--- Sources/HTTP/Request/Verb.swift
+++ Sources/HTTP/Request/Verb.swift
@@ -1,4 +1,16 @@
 public enum Verb: String {
-	case get = "GET"
-	case post = "POST"
+	// Source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+
+	case get     = "GET"     // download
+	case head    = "HEAD"    // get headers, but not data
+
+	case post    = "POST"    // add new
+	case put     = "PUT"     // replace
+	case patch   = "PATCH"   // modify partially
+
+	case delete  = "DELETE"  // remove
+
+	case connect = "CONNECT" // establish connection (e.g. TLS handshake)
+	case options = "OPTIONS" // called by browsers to establish CORS
+	case trace   = "TRACE"   // debugging call (used by trace-route I guess)
 }
