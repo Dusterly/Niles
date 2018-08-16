538e1e85340a327f6fa75463d490dc66dc03f645
Johan Segerfeldt <johan.segerfeldt@me.com>
2018-08-16 22:11:54 +0200

    Add bodies to responses

--- Sources/Routing/Router.swift
+++ Sources/Routing/Router.swift
@@ -13,14 +13,27 @@ public class Router {
 	}
 
 	public func response(routing request: Request) -> Response {
-		guard let route = routes[request.path] else { return AutomaticResponse(statusCode: .notFound) }
-		guard let handle = route[request.verb] else { return AutomaticResponse(statusCode: .methodNotAllowed) }
+		guard let route = routes[request.path] else {
+			return AutomaticResponse(
+				statusCode: .notFound,
+				body: "No resource exists for path '\(request.path)'."
+			)
+		}
+		guard let handle = route[request.verb] else {
+			return AutomaticResponse(
+				statusCode: .methodNotAllowed,
+				body: "Method \(request.verb) is not supported for \(request.path)"
+			)
+		}
 		do {
 			return try handle(request) ?? AutomaticResponse(statusCode: .noContent)
 		} catch let response as Response where response.statusCode.isError {
 			return response
 		} catch {
-			return AutomaticResponse(statusCode: .internalServerError)
+			return AutomaticResponse(
+				statusCode: .internalServerError,
+				body: error.localizedDescription
+			)
 		}
 	}
 }
@@ -28,5 +41,10 @@ public class Router {
 struct AutomaticResponse: Response {
 	var statusCode: StatusCode
 	var headers: [String : String] { return [:] }
-	var body: Data? { return nil }
+	var body: Data?
+
+	init(statusCode: StatusCode, body: String? = nil) {
+		self.statusCode = statusCode
+		self.body = body?.data(using: .utf8)
+	}
 }
