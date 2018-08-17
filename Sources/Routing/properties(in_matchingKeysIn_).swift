53ece526b744a9e2da439321a05e2d30ca27ae0e
Johan Segerfeldt <johan.segerfeldt@me.com>
2018-08-17 21:16:50 +0200

    Massively reduce properties(in:, matchingKeysIn:)

--- Sources/Routing/properties(in_matchingKeysIn_).swift
+++ Sources/Routing/properties(in_matchingKeysIn_).swift
@@ -1,55 +1,51 @@
 import Foundation
 
-private let regex = try! NSRegularExpression(pattern: "\\{(.*?)\\}|[^{}]+")
-
 public func properties(in path: String, matchingKeysIn pattern: String) -> [String: String]? {
 	var properties: [String: String] = [:]
+	var currentPathIndex = path.startIndex
 
-	let matches = regex.matches(in: pattern, range: NSRange(pattern.startIndex..., in: pattern)).map { (it: NSTextCheckingResult) -> Match in
-		var groupRanges: [Range<String.Index>?] = []
-		for i in 0..<it.numberOfRanges { groupRanges.append(it.range(at: i, in: pattern)) }
-		let entireRange = it.range(in: pattern)!
-		return Match(
-			entireRange: entireRange,
-			groupRanges: groupRanges,
-			entireMatch: String(pattern[entireRange]),
-			groups: groupRanges.map({ if let it = $0 { return String(pattern[it]) } else { return nil } })
-		)
+	let components = Routing.components(in: pattern)
+	let endIndexFinders: [() -> String.Index?] = components.dropFirst().map { component in
+		{ path.range(of: component.text, range: currentPathIndex..<path.endIndex)?.lowerBound }
 	}
 
-	var currentPathIndex = path.startIndex
-	for (match, next) in zip(matches, matches.dropFirst()) {
-		if let key = match.groups[1] {
-			guard let nextIndex = path.range(of: next.entireMatch, range: currentPathIndex..<path.endIndex)?.lowerBound else { return nil }
-
-			let value = String(path[currentPathIndex..<nextIndex])
+	for (component, findNextEndIndex) in zip(components, endIndexFinders + [ { path.endIndex }]) {
+		if let key = component.key {
+			guard let valueEndIndex = findNextEndIndex() else { return nil }
+			let value = String(path[currentPathIndex..<valueEndIndex])
 			properties[key] = value
 		} else {
-			let textRange = match.entireRange
-			guard let range = path.range(of: pattern[textRange], range: currentPathIndex..<path.endIndex) else { return nil }
+			guard let range = path.range(of: component.text, range: currentPathIndex..<path.endIndex) else { return nil }
 			currentPathIndex = range.upperBound
 		}
-	}
-
-	let match = matches.last!
-
-	if let key = match.groups[1] {
-		let value = String(path[currentPathIndex...])
-		properties[key] = value
-	} else {
-		let textRange = match.entireRange
-		if path.range(of: pattern[textRange], range: currentPathIndex..<path.endIndex) == nil { return nil }
 	}
 
 	return properties
 }
 
-private struct Match {
-	var entireRange: Range<String.Index>
-	var groupRanges: [Range<String.Index>?]
+private func components(in pattern: String) -> [Component] {
+	let regex = try! NSRegularExpression(pattern: "\\{(.*?)\\}|[^{}]+")
+	return regex.matches(in: pattern).map {
+		Component(
+			text: String(pattern[$0.range(in: pattern)!]),
+			key: $0.range(at: 1, in: pattern).flatMap { String(pattern[$0]) }
+		)
+	}
+}
 
-	var entireMatch: String
-	var groups: [String?]
+private struct Component {
+	var text: String
+	var key: String?
+}
+
+private extension NSRegularExpression {
+	func matches(in string: String) -> [NSTextCheckingResult] {
+		return matches(in: string, range: string.startIndex..<string.endIndex)
+	}
+
+	func matches(in string: String, range: Range<String.Index>) -> [NSTextCheckingResult] {
+		return matches(in: string, range: NSRange(range, in: string))
+	}
 }
 
 private extension NSTextCheckingResult {
