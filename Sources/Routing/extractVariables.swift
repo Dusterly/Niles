8c264cf2efb113b52aa43908b3a45fadba58a146
Johan Segerfeldt <johan.segerfeldt@me.com>
2018-08-17 14:29:56 +0200

    Extract struct Match

--- Sources/Routing/extractVariables.swift
+++ Sources/Routing/extractVariables.swift
@@ -6,35 +6,51 @@ public func extractVariables(from path: String, matching pattern: String) -> [St
 	var variables: [String: String] = [:]
 
 	let matches = regex.matches(in: pattern, range: NSRange(pattern.startIndex..., in: pattern))
+	let matches0 = matches.map { (it: NSTextCheckingResult) -> Match in
+		var groupRanges: [Range<String.Index>?] = []
+		for i in 0..<it.numberOfRanges { groupRanges.append(it.range(at: i, in: pattern)) }
+		let entireRange = it.range(in: pattern)!
+		return Match(
+			entireRange: entireRange,
+			groupRanges: groupRanges,
+			entireMatch: String(pattern[entireRange]),
+			groups: groupRanges.map({ if let it = $0 { return String(pattern[it]) } else { return nil } })
+		)
+	}
 
 	var currentPathIndex = path.startIndex
-	for (match, next) in zip(matches, matches.dropFirst()) {
-		if let keyRange = match.range(at: 1, in: pattern) {
-			let key = String(pattern[keyRange])
-
-			guard let nextIndex = path.range(of: pattern[next.range(in: pattern)!], range: currentPathIndex..<path.endIndex)?.lowerBound else { return nil }
+	for (match, next) in zip(matches0, matches0.dropFirst()) {
+		if let key = match.groups[1] {
+			guard let nextIndex = path.range(of: next.entireMatch, range: currentPathIndex..<path.endIndex)?.lowerBound else { return nil }
 
 			let value = String(path[currentPathIndex..<nextIndex])
 			variables[key] = value
 		} else {
-			let textRange = match.range(in: pattern)!
+			let textRange = match.entireRange
 			guard let range = path.range(of: pattern[textRange], range: currentPathIndex..<path.endIndex) else { return nil }
 			currentPathIndex = range.upperBound
 		}
 	}
 
-	let match = matches.last!
+	let match = matches0.last!
 
-	if let keyRange = match.range(at: 1, in: pattern) {
-		let key = String(pattern[keyRange])
+	if let key = match.groups[1] {
 		let value = String(path[currentPathIndex...])
 		variables[key] = value
 	} else {
-		let textRange = match.range(in: pattern)!
+		let textRange = match.entireRange
 		if path.range(of: pattern[textRange], range: currentPathIndex..<path.endIndex) == nil { return nil }
 	}
 
 	return variables
+}
+
+private struct Match {
+	var entireRange: Range<String.Index>
+	var groupRanges: [Range<String.Index>?]
+
+	var entireMatch: String
+	var groups: [String?]
 }
 
 private extension NSTextCheckingResult {
