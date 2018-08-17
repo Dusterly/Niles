ae865bcd3732bc489880093313306e7242d36208
Johan Segerfeldt <johan.segerfeldt@me.com>
2018-08-17 07:23:12 +0200

    Extract extension for NSTextCheckingResult

--- Sources/Routing/extractVariables.swift
+++ Sources/Routing/extractVariables.swift
@@ -9,15 +9,15 @@ public func extractVariables(from path: String, matching pattern: String) -> [St
 
 	var currentPathIndex = path.startIndex
 	for (match, next) in zip(matches, matches.dropFirst()) {
-		if let keyRange = Range(match.range(at: 1), in: pattern) {
+		if let keyRange = match.range(at: 1, in: pattern) {
 			let key = String(pattern[keyRange])
 
-			guard let nextIndex = path.range(of: pattern[Range(next.range, in: pattern)!], range: currentPathIndex..<path.endIndex)?.lowerBound else { return nil }
+			guard let nextIndex = path.range(of: pattern[next.range(in: pattern)!], range: currentPathIndex..<path.endIndex)?.lowerBound else { return nil }
 
 			let value = String(path[currentPathIndex..<nextIndex])
 			variables[key] = value
 		} else {
-			let textRange = Range(match.range, in: pattern)!
+			let textRange = match.range(in: pattern)!
 			guard let range = path.range(of: pattern[textRange], range: currentPathIndex..<path.endIndex) else { return nil }
 			currentPathIndex = range.upperBound
 		}
@@ -25,14 +25,24 @@ public func extractVariables(from path: String, matching pattern: String) -> [St
 
 	let match = matches.last!
 
-	if let keyRange = Range(match.range(at: 1), in: pattern) {
+	if let keyRange = match.range(at: 1, in: pattern) {
 		let key = String(pattern[keyRange])
 		let value = String(path[currentPathIndex...])
 		variables[key] = value
 	} else {
-		let textRange = Range(match.range, in: pattern)!
+		let textRange = match.range(in: pattern)!
 		if path.range(of: pattern[textRange], range: currentPathIndex..<path.endIndex) == nil { return nil }
 	}
 
 	return variables
+}
+
+private extension NSTextCheckingResult {
+	func range(in string: String) -> Range<String.Index>? {
+		return Range(range, in: string)
+	}
+
+	func range(at index: Int, in string: String) -> Range<String.Index>? {
+		return Range(range(at: index), in: string)
+	}
 }
